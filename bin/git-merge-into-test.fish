007beac2f889e0c3219fa4e04270d525e4ea4535
bangbang93 <bangbang93@163.com>
2025-08-26 14:52:13 +0800

    fix: 合并前先pull一下

--- bin/git-merge-into-test.fish
+++ bin/git-merge-into-test.fish
@@ -6,8 +6,8 @@
 set current_branch (git rev-parse --abbrev-ref HEAD)
 
 # 2. 检查当前分支是否为 develop
-if test "$current_branch" = "develop"
-    echo "当前分支为 develop，无法合并到 develop"
+if test "$current_branch" != "develop"
+    echo "当前分支不是 develop"
     exit 1
 end
 
@@ -17,25 +17,25 @@ if test (git status --porcelain | wc -l) -ne 0
     exit 1
 end
 
-# 4. 切换到 develop 分支
-git switch develop
+# 4. 切换到 test 分支
+git switch test
 
 git pull
 
-# 5. 合并到 develop 分支
+# 5. 合并到 test 分支
 git merge $current_branch
 if test $status -ne 0
     echo "合并有冲突，请解决冲突后再提交"
     exit 1
 end
 
-# 6. 推送到远程 develop 分支
-git push origin develop
+# 6. 推送到远程 test 分支
+git push origin test
 if test $status -ne 0
-    echo "推送到远程 develop 分支失败，请检查"
+    echo "推送到远程 test 分支失败，请检查"
     exit 1
 end
-echo "成功将当前分支 '$current_branch' 合并到 develop 分支并推送到远程"
+echo "成功将当前分支 '$current_branch' 合并到 test 分支并推送到远程"
 
 # 7. 切换回原来的分支
 git switch $current_branch
